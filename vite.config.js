de305b043f24301bf31c7fb48672d867ad039f25
Rohitrrr384 <rohitrathod15032004@gmail.com>
2025-05-26 13:43:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 import { terser } from 'rollup-plugin-terser'; // For JavaScript minification
 
 export default defineConfig({
-  base: "/portfolio/", // ✅ Add this line: your repo name
-
   plugins: [
     react(),
     process.env.NODE_ENV === "production" && terser(),
